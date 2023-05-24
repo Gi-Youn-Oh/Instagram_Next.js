16978801abc35b01acbe3033acdda1e43f1865c7
Giyoun Oh <okrldbs@naver.com>
2023-05-25 01:29:24 +0900

    sanity DB adding

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,3 +1,4 @@
+import { addUser } from '@/service/user';
 import NextAuth, { NextAuthOptions } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 export const authOptions: NextAuthOptions = {
@@ -10,6 +11,14 @@ export const authOptions: NextAuthOptions = {
         // ...add more providers here
     ],
     callbacks: {
+        async signIn({user: {id, name, email, image}}){
+            // console.log('signIn', user);
+            if(!email){
+                return false;
+            }
+            addUser({id, name: name || '', image, email, username: email.split('@')[0]});
+            return true;
+        },
         async session({ session }) {
             // Send properties to the client, like an access_token and user id from a provider.
             console.log('session', session);
