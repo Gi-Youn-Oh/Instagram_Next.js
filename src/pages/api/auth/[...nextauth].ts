39432449a4b940fdd10de78f99f12b755370b8c7
Giyoun Oh <okrldbs@naver.com>
2023-06-08 21:15:29 +0900

    GROQ query

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,13 +2,11 @@ import { addUser } from '@/service/user';
 import NextAuth, { NextAuthOptions } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 export const authOptions: NextAuthOptions = {
-    // Configure one or more authentication providers
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_OAUTH_ID || '',
             clientSecret: process.env.GOOGLE_OAUTH_SECRET || '',
         }),
-        // ...add more providers here
     ],
     callbacks: {
         async signIn({user: {id, name, email, image}}){
@@ -21,7 +19,7 @@ export const authOptions: NextAuthOptions = {
         },
         async session({ session }) {
             // Send properties to the client, like an access_token and user id from a provider.
-            console.log('session', session);
+            // console.log('session', session);
             const user = session?.user;
             if(user){
                 session.user = {
