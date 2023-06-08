ff3d308ab620dd2a6e01e29b2a3b6fb94324ac40
Giyoun Oh <okrldbs@naver.com>
2023-06-08 22:10:01 +0900

    Carousel Bar

--- src/components/FollowingBar.tsx
+++ src/components/FollowingBar.tsx
@@ -1,38 +1,44 @@
-'use client'
-
+'use client';
 import { DetailUser } from '@/model/user';
 import Link from 'next/link';
 import { PropagateLoader } from 'react-spinners';
-import useSWR from 'swr'
+import useSWR from 'swr';
 import Avatar from './Avatar';
+import ScrollableBar from './ui/scrollableBar';
 
 export default function FollowingBar() {
-    const { data, isLoading: loading, error } = useSWR<DetailUser>('/api/me')
-    // const users = data?.following;
-    // const users = undefined;
-    const users = data?.following && [...data?.following,
-    ...data?.following
-        , ...data?.following
-        , ...data?.following
-        , ...data?.following
-    ]
-
-    return <section className='w-full flex justify-center items-center p-4 shadow-sm shadow-neutral-300 mb-4 rounded-lg min-h-[90px] overflow-x-auto'>
-        {loading ? <PropagateLoader size={8} color='red' /> :
-            (!users || users.length === 0) && <p>{`You don't have followings`}</p>}
-        {
-            users && users.length > 0 &&
-            <ul className='w-full flex gap-2 '>
-                {users.map(({ image, username }) =>
-                    <li key={username}>
-                        <Link
-                            className='flex flex-col items-center w-20'
-                            href={`/user/${username}`}>
-                            <Avatar image={image} hightlight />
-                            <p className='w-full text-sm text-center text-ellipsis overflow-hidden'>{username}</p>
-                        </Link>
-                    </li>)}
-            </ul>
-        }
+  const { data, isLoading: loading, error } = useSWR<DetailUser>('/api/me');
+  // const users = data?.following;
+  // const users = undefined;
+  const users = data?.following && [
+    ...data?.following,
+    ...data?.following,
+    ...data?.following,
+    ...data?.following,
+  ];
+  return (
+    <section className='w-full flex justify-center items-center p-4 shadow-sm shadow-neutral-300 mb-4 rounded-lg min-h-[90px] overflow-x-auto'>
+      {loading ? (
+        <PropagateLoader size={8} color='red' />
+      ) : (
+        (!users || users.length === 0) && <p>{`You don't have following`}</p>
+      )}
+      {users && users.length > 0 && (
+        <ScrollableBar>
+          {users.map(({ image, username }) => (
+            <Link
+              key={username}
+              className='flex flex-col items-center w-20'
+              href={`/user/${username}`}
+            >
+              <Avatar image={image} hightlight />
+              <p className='w-full text-sm text-center text-ellipsis overflow-hidden'>
+                {username}
+              </p>
+            </Link>
+          ))}
+        </ScrollableBar>
+      )}
     </section>
+  );
 }
