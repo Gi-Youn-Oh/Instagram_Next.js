b059a78d8a8eeec22eac0bc25ac70906a867b57f
Giyoun Oh <okrldbs@naver.com>
2023-06-10 21:01:11 +0900

    SEO Optimize

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -3,6 +3,12 @@ import { authOptions } from '@/pages/api/auth/[...nextauth]';
 import { getServerSession } from 'next-auth';
 import { getProviders } from 'next-auth/react';
 import { redirect } from 'next/navigation';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: "Signin",
+  description: 'Signup or Login to GY"s Instagram',
+};
 
 type Props = {
   searchParams: {
--- src/app/user/[username]/page.tsx
+++ src/app/user/[username]/page.tsx
@@ -2,6 +2,8 @@ import UserPosts from "@/components/UserPosts";
 import UserProfile from "@/components/UserProfile";
 import { getUserForProfile } from "@/service/user";
 import { notFound } from "next/navigation";
+import { Metadata } from 'next';
+import { cache } from "react";
 
 type Props = {
     params: {
@@ -9,10 +11,12 @@ type Props = {
     }
 }
 
+const getUser = cache(async (username: string) => getUserForProfile(username))
+
 export default async function UserPage({ params: { username } }: Props) {
     // 상단 : 사용자의 프로필 이미지와 정보(username, name, 숫자)
     // 하단 : 3개의 탭 (posts, liked, bookmarks)
-    const user = await getUserForProfile(username);
+    const user = await getUser(username);
 
     if (!user) {
         notFound();
@@ -24,4 +28,12 @@ export default async function UserPage({ params: { username } }: Props) {
             <UserPosts user={user} />
         </section>
     )
+}
+
+export async function generateMetadata({ params: { username } }: Props): Promise<Metadata> {
+    const user = await getUser(username);
+    return {
+        title: `${user?.name} (@${user?.username}) • Instagram Photos`,
+        description: `${user?.name}'s all Instagram posts`,
+    }
 }
