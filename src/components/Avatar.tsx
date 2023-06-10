ddbf3fca24b5dfd1d4f7c4673e6dd4930516af93
Giyoun Oh <okrldbs@naver.com>
2023-06-10 19:25:00 +0900

    UserProfile Part1

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,4 +1,4 @@
-type AvatarSize = 'small' | 'medium' | 'large';
+type AvatarSize = 'small' | 'medium' | 'large' | 'xlarge';
 
 type Props = {
     image?: string | null;
@@ -6,33 +6,74 @@ type Props = {
     hightlight?: boolean;
 };
 
-export default function Avatar({ image, size='large', hightlight=false }: Props) {
+export default function Avatar({ image, size = 'large', hightlight = false }: Props) {
 
     return <div className={getContainerStyle(size, hightlight)}>
         {/*eslint-disable-next-line @next/next/no-img-element */}
-        <img className={`bg-white object-cover rounded-full p-[0.1rem] ${getImageSizeStyle(size)}`} alt='user profile' src={image ?? undefined} referrerPolicy="no-referrer"></img>
+        <img className={`bg-white object-cover rounded-full
+        ${getImageSizeStyle(size).image}`}
+            alt='user profile' src={image ?? undefined} referrerPolicy="no-referrer"></img>
     </div>
 }
-"w-9 h-9 rounded-full "
-function getContainerStyle(size: AvatarSize, hightlight: boolean): string {
+
+function getContainerStyle(size: AvatarSize, highlight: boolean): string {
     const baseStyle = 'rounded-full flex justify-center items-center';
-    const hightlightStyle = hightlight? 'bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300' : '';
-    const sizeStyle = getContainerSize(size);
-    return `${baseStyle} ${hightlightStyle} ${sizeStyle}` ;
-}
+    const highlightStyle = highlight
+      ? 'bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300'
+      : '';
+    const { container } = getImageSizeStyle(size);
+    return `${baseStyle} ${highlightStyle} ${container}`;
+  }
 
-function getContainerSize(size: AvatarSize): string {
-    switch (size) {
-        case 'small' : return 'w-9 h-9';
-        case 'medium' : return 'w-11 h-11';
-        case 'large' : return 'w-[68px] h-[68px]';
-    }
-}
+// function getContainerSize(size: AvatarSize): string {
+//     switch (size) {
+//         case 'small': return 'w-9 h-9';
+//         case 'medium': return 'w-11 h-11';`
+//         case 'large': return 'w-[68px] h-[68px]';
+//         case 'xlarge': return 'w-[142px] h-[142px]';
+//         default:
+//             throw new Error(`Unsupported type size: ${size}`)
+//     }
+// }
 
-function getImageSizeStyle (size: AvatarSize): string {
+// function getImageSizeStyle(size: AvatarSize): string {
+//     switch (size) {
+//         case 'small': return 'w-[34px] h-[34px] p-[0.1rem]';
+//         case 'medium': return 'w-[42px] h-[42px] p-[0.1rem]';
+//         case 'large': return 'w-16 h-16 p-[0.2rem]';
+//         case 'xlarge': return 'w-[138px] h-[138px] p-[0.3rem]'
+//         default:
+//             throw new Error(`Unsupported type size: ${size}`)
+//     }
+// }
+
+type ImageSizeStyle = {
+    container: string;
+    image: string;
+  };
+  function getImageSizeStyle(size: AvatarSize): ImageSizeStyle {
     switch (size) {
-        case 'small' : return 'w-[34px] h-[34px] p-[0.1rem]';
-        case 'medium' : return 'w-[42px] h-[42px] p-[0.1rem]';
-        case 'large' : return 'w-16 h-16 p-[0.2rem]';
+      case 'small':
+        return {
+          container: 'w-9 h-9',
+          image: 'w-[34px] h-[34px] p-[0.1rem]',
+        };
+      case 'medium':
+        return {
+          container: 'w-11 h-11',
+          image: 'w-[42px] h-[42px] p-[0.1rem]',
+        };
+      case 'large':
+        return {
+          container: 'w-[68px] h-[68px]',
+          image: 'w-16 h-16 p-[0.2rem]',
+        };
+      case 'xlarge':
+        return {
+          container: 'w-[142px] h-[142px]',
+          image: 'w-[138px] h-[138px] p-[0.3rem]',
+        };
+      default:
+        throw new Error(`Unsupported type size: ${size}`);
     }
-}
+  }
