af305477d37313a032e0ddf7ea3190c2340983be
Giyoun Oh <okrldbs@naver.com>
2023-06-11 15:40:23 +0900

    Route Handler

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -17,7 +17,7 @@ export const authOptions: NextAuthOptions = {
             addUser({id, name: name || '', image, email, username: email.split('@')[0]});
             return true;
         },
-        async session({ session }) {
+        async session({ session, token }) {
             // Send properties to the client, like an access_token and user id from a provider.
             // console.log('session', session);
             const user = session?.user;
@@ -25,9 +25,16 @@ export const authOptions: NextAuthOptions = {
                 session.user = {
                     ...user,
                     username: user.email?.split('@')[0] || '',
+                    id:token.id as string,
                 }
             }
             return session
+        },
+        async jwt({token, user}) {
+            if(user) {
+                token.id = user.id;
+            }
+            return token;
         }
     },
     pages: {
