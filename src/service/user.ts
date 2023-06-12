d74adda9a87d8165e91ce0f2e0d203b3e66349a6
Giyoun Oh <okrldbs@naver.com>
2023-06-12 20:05:47 +0900

    Follow Complete

--- src/service/user.ts
+++ src/service/user.ts
@@ -50,17 +50,23 @@ export async function searchUsers(keyword?: string) {
 }
 
 export async function getUserForProfile(username: string) {
-    return client.fetch(
-        `*[_type == "user" && username == "${username}"][0]{
-            ...,
-            "id":_id,
-            "following": count(following),
-            "followers": count(following),
-            "posts": count(*[_type == "post" && author->username == "${username}"])
-        }`
-    ).then(user => ({
-        ...user, following: user.following ?? 0, followers: user.followers ?? 0, posts: user.posts ?? 0
-    }))
+    return client
+        .fetch(
+            `*[_type == "user" && username == "${username}"][0]{
+        ...,
+        "id":_id,
+        "following": count(following),
+        "followers": count(followers),
+        "posts": count(*[_type=="post" && author->username == "${username}"])
+      }
+      `
+        )
+        .then((user) => ({
+            ...user,
+            following: user.following ?? 0,
+            followers: user.followers ?? 0,
+            posts: user.posts ?? 0,
+        }));
 }
 
 export async function addBookmark(userId: string, postId: string) {
@@ -81,15 +87,25 @@ export async function removeBookmark(userId: string, postId: string) {
 }
 
 export async function follow(myId: string, targetId: string) {
-    return client.transaction()
-        .patch(myId, (user) => user.setIfMissing({ following: [] }).append('following', [{ _ref: targetId, _type: 'reference' }]))
-        .patch(targetId, (user) => user.setIfMissing({ followers: [] }).append('followers', [{ _ref: myId, _type: 'reference' }]))
-        .commit({ autoGenerateArrayKeys: true })
+    return client
+        .transaction() //
+        .patch(myId, (user) =>
+            user
+                .setIfMissing({ following: [] })
+                .append('following', [{ _ref: targetId, _type: 'reference' }])
+        )
+        .patch(targetId, (user) =>
+            user
+                .setIfMissing({ followers: [] })
+                .append('followers', [{ _ref: myId, _type: 'reference' }])
+        )
+        .commit({ autoGenerateArrayKeys: true });
 }
 
 export async function unfollow(myId: string, targetId: string) {
-    return client.transaction()
+    return client
+        .transaction() //
         .patch(myId, (user) => user.unset([`following[_ref=="${targetId}"]`]))
         .patch(targetId, (user) => user.unset([`followers[_ref=="${myId}"]`]))
-        .commit({ autoGenerateArrayKeys: true })
+        .commit({ autoGenerateArrayKeys: true });
 }
