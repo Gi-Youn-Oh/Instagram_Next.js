709bacc7b4bc8cfbf506c542ed4c0312e0939558
Giyoun Oh <okrldbs@naver.com>
2023-06-12 13:25:59 +0900

    Comment Part2

--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -5,6 +5,8 @@ import PostUserAvatar from "./PostUserAvatar";
 import ActionBar from "./ActionBar";
 import CommentForm from "./CommentForm";
 import Avatar from "./Avatar";
+import useFullPost from "@/hooks/post";
+import useMe from "@/hooks/me";
 
 type Props = {
     post: SimplePost;
@@ -12,30 +14,35 @@ type Props = {
 
 export default function PostDetail({ post }: Props) {
     const { id, userImage, username, image, createdAt, likes } = post;
-    const { data } = userSWR<FullPost>(`/api/posts/${id}`);
+    const { post: data, postComment } = useFullPost(id);
+    const { user } = useMe();
     const comments = data?.comments;
-    // console.log('comments', comments);
+
+    const handlePostComment = (comment: string) => {
+        user && postComment({ comment, username: user.username, image: user.image });
+    }
+
     return <section className="flex w-full h-full">
         <div className="relative basis-3/5">
-            <Image 
-            className="object-cover"
-            src={image} alt={`photo by ${username}`} priority fill sizes="650px" />
+            <Image
+                className="object-cover"
+                src={image} alt={`photo by ${username}`} priority fill sizes="650px" />
         </div>
         <div className="w-full basis-2/5 flex flex-col">
             <PostUserAvatar image={userImage} username={username} />
             <ul className="border-t border-gray-200 h-full overflow-y-auto p-4 mb-1">
-                {comments && comments.map(({ image, username: commentUsername, comment }, index) => 
-                <li className="flex items-center mb-1" 
-                key={index}>
-                    <Avatar image={image} size="small" hightlight={commentUsername === username} />
-                    <div className="ml-2">
-                        <span className="font-bold mr-1">{commentUsername}</span>
-                        <span>{comment}</span>
-                    </div>
-                </li>)}
+                {comments && comments.map(({ image, username: commentUsername, comment }, index) =>
+                    <li className="flex items-center mb-1"
+                        key={index}>
+                        <Avatar image={image} size="small" hightlight={commentUsername === username} />
+                        <div className="ml-2">
+                            <span className="font-bold mr-1">{commentUsername}</span>
+                            <span>{comment}</span>
+                        </div>
+                    </li>)}
             </ul>
             <ActionBar post={post} />
-            <CommentForm />
+            <CommentForm onPostComment={handlePostComment} />
         </div>
     </section>
 }
--- src/components/PostListCard.tsx
+++ src/components/PostListCard.tsx
@@ -10,6 +10,7 @@ import ModalPortal from "./ui/ModalPortal";
 import PostMordal from "./PostMordal";
 import PostDetail from "./PostDetail";
 import PostUserAvatar from "./PostUserAvatar";
+import usePosts from "@/hooks/posts";
 
 type Props = {
     post: SimplePost;
@@ -19,6 +20,11 @@ type Props = {
 export default function PostListCard({ post, priority = false }: Props) {
     const { username, userImage, image, comments, text } = post;
     const [openMordal, setOpenMordal] = useState(false);
+    const { postComment } = usePosts();
+
+    const handlePostComment = (comment: string) => {
+        postComment(post, comment);
+    }
 
     return (
         <article className="rounded-lg shadow-md border border-gray-200">
@@ -32,11 +38,11 @@ export default function PostListCard({ post, priority = false }: Props) {
                     <span className='font-bold mr-1'>{username}</span>
                     {text}
                 </p>
-                {comments > 1 && <button 
-                className="font-bold my-2 text-sky-500"
-                onClick={() => setOpenMordal(true)}>{`View all ${comments} comments`}</button>}
+                {comments > 1 && <button
+                    className="font-bold my-2 text-sky-500"
+                    onClick={() => setOpenMordal(true)}>{`View all ${comments} comments`}</button>}
             </ActionBar>
-            <CommentForm />
+            <CommentForm onPostComment={handlePostComment} />
             {
                 openMordal && <ModalPortal>
                     <PostMordal onClose={() => setOpenMordal(false)}>
