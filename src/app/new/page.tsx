39472ee368d6e2f4a73955c03c34b6e17f30e5e6
Giyoun Oh <okrldbs@naver.com>
2023-06-13 12:37:50 +0900

    Post Upload Drag

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,3 +1,20 @@
-export default function NewPage() {
-    return <h1>New Page</h1>;
+import NewPost from "@/components/NewPost";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import { Metadata } from "next";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+
+export const metadata: Metadata = {
+    title: 'New Post',
+    description: 'Create a new post',
+}
+
+export default async function NewPostPage() {
+    const session = await getServerSession(authOptions);
+    
+    if (!session?.user) {
+        redirect('/auth/singin');
+    }
+
+    return <NewPost user={session.user} />
 }
